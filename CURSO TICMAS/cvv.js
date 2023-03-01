e94a80c5f2f934841a42894b0daa3a9daa8abd0a
tomasitogh <126632197+tomasitogh@users.noreply.github.com>
2023-02-28 22:24:57 -0300

    Add files via upload

---
+++ CURSO TICMAS/cvv.js
@@ -0,0 +1,10 @@
+/*  
+$.ajax({
+    url: 'https://randomuser.me/api/',
+    dataType: 'json',
+    success: function(data) {
+      console.log(data);
+    }
+  });*/
+  
+  /* lo puse en comentario porque no cambia en nada el archivo */
